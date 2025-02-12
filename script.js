dec9b4e2fed563c05f0a8d40bc45e6c1c57c65f4
pJayaFps <tititimoteo455@gmail.com>
2025-02-12 15:52:46 -0300

    Atualização Básica 3.0

--- script.js
+++ script.js
@@ -8,3 +8,18 @@ function menuShow() {
         document.querySelector('.icon').src = "assets/close_white_36dp.svg";
     }
 }
+
+const parceriasLink = document.getElementById('parcerias');
+const box = document.getElementById('box')
+
+parceriasLink.addEventListener('mouseover', function() {
+    box.style.display = 'block'; // Exibe a box
+});
+
+// Função para ocultar a box com transição
+parceriasLink.addEventListener('mouseout', function() {
+    setTimeout(() => {
+        box.style.display = 'none';
+    }, 5000);
+});
+
